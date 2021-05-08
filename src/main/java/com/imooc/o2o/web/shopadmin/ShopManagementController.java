20563baedc0ecf746b586f658b12059a1f83abf4
Vicki-Wang <59798082+Vicki-Wang@users.noreply.github.com>
2021-05-08 17:39:43 +0800

    GetShopInitInfo

--- src/main/java/com/imooc/o2o/web/shopadmin/ShopManagementController.java
+++ src/main/java/com/imooc/o2o/web/shopadmin/ShopManagementController.java
@@ -1,10 +1,9 @@
 package com.imooc.o2o.web.shopadmin;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,85 +12,115 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.imooc.o2o.dto.ShopExecution;
+import com.imooc.o2o.entity.Area;
 import com.imooc.o2o.entity.PersonInfo;
 import com.imooc.o2o.entity.Shop;
+import com.imooc.o2o.entity.ShopCategory;
 import com.imooc.o2o.enums.ShopStateEnum;
 import com.imooc.o2o.exceptions.ShopOperationException;
+import com.imooc.o2o.service.AreaService;
+import com.imooc.o2o.service.ShopCategoryService;
 import com.imooc.o2o.service.ShopService;
 import com.imooc.o2o.util.HttpServletRequestUtil;
-import com.imooc.o2o.util.ImageUtil;
-import com.imooc.o2o.util.PathUtil;
 
 @Controller
 @RequestMapping("/shopadmin")
 public class ShopManagementController {
-	
+
 	@Autowired
 	private ShopService shopService;
-	
-	@RequestMapping(value = "/registershop",method = RequestMethod.POST)
-	private Map<String,Object> registerShop(HttpServletRequest request){
-		Map<String,Object> modelMap = new HashMap<String,Object>();
-		//1.接收并转化相应 的参数，包括店铺信息以及图片信息
+
+	@Autowired
+	private ShopCategoryService shopCategoryService;
+
+	@Autowired
+	private AreaService areaService;
+
+	@RequestMapping(value = "/getshopinitinfo", method = RequestMethod.GET)
+	@ResponseBody
+	private Map<String, Object> getShopInitInfo() {
+		Map<String, Object> modelMap = new HashMap<String, Object>();
+		List<ShopCategory> shopCategoryList = new ArrayList<ShopCategory>();
+		List<Area> areaList = new ArrayList<Area>();
+		try {
+			shopCategoryList = shopCategoryService.getShopCategoryList(new ShopCategory());
+			areaList = areaService.getAreaList();
+			modelMap.put("shopCategoryList", shopCategoryList);
+			modelMap.put("areaList", areaList);
+			modelMap.put("success", true);
+		} catch (Exception e) {
+			modelMap.put("success", false);
+			modelMap.put("errMsg", e.getMessage());
+		}
+
+		return modelMap;
+
+	}
+
+	@RequestMapping(value = "/registershop", method = RequestMethod.POST)
+	private Map<String, Object> registerShop(HttpServletRequest request) {
+		Map<String, Object> modelMap = new HashMap<String, Object>();
+		// 1.接收并转化相应 的参数，包括店铺信息以及图片信息
 		String shopStr = HttpServletRequestUtil.getString(request, "shopStr");
 		ObjectMapper mapper = new ObjectMapper();
 		Shop shop = null;
 		try {
-			shop = mapper.readValue(shopStr,Shop.class);
-		}catch(Exception e) {
-			modelMap.put("success",false);
-			modelMap.put("errMsg",e.getMessage());
+			shop = mapper.readValue(shopStr, Shop.class);
+		} catch (Exception e) {
+			modelMap.put("success", false);
+			modelMap.put("errMsg", e.getMessage());
 			return modelMap;
 		}
 		CommonsMultipartFile shopImg = null;
-		CommonsMultipartResolver commonsMulrtipartResolver = new CommonsMultipartResolver(request.getSession().getServletContext());
-		if(commonsMulrtipartResolver.isMultipart(request)) {
-			MultipartHttpServletRequest multipartHttpServletRequest = (MultipartHttpServletRequest)request;
-			shopImg = (CommonsMultipartFile)multipartHttpServletRequest.getFile("shopImg");
-		}else {
+		CommonsMultipartResolver commonsMulrtipartResolver = new CommonsMultipartResolver(
+				request.getSession().getServletContext());
+		if (commonsMulrtipartResolver.isMultipart(request)) {
+			MultipartHttpServletRequest multipartHttpServletRequest = (MultipartHttpServletRequest) request;
+			shopImg = (CommonsMultipartFile) multipartHttpServletRequest.getFile("shopImg");
+		} else {
 			modelMap.put("success", false);
 			modelMap.put("errMsg", "上传图片不能为空");
 			return modelMap;
 		}
-		//2.注册店铺
-		if(shop!=null&&shopImg != null) {
+		// 2.注册店铺
+		if (shop != null && shopImg != null) {
 			PersonInfo owner = new PersonInfo();
-			//Session TODO
+			// Session TODO
 			owner.setUserId(1L);
 			shop.setOwner(owner);
-			File shopImgFile = new File(PathUtil.getImgBasePath()+ImageUtil.getRandomFileName());
 			ShopExecution se;
 			try {
-				se = shopService.addShop(shop, shopImg.getInputStream(),shopImg.getOriginalFilename());
-				if(se.getState() == ShopStateEnum.CHECK.getState()) {
+				se = shopService.addShop(shop, shopImg.getInputStream(), shopImg.getOriginalFilename());
+				if (se.getState() == ShopStateEnum.CHECK.getState()) {
 					modelMap.put("success", true);
-				}else {
-					modelMap.put("success", false);	
+				} else {
+					modelMap.put("success", false);
 					modelMap.put("errMsg", se.getStateInfo());
 				}
-			}catch(ShopOperationException e) {
-				modelMap.put("success", false);	
+			} catch (ShopOperationException e) {
+				modelMap.put("success", false);
 				modelMap.put("errMsg", e.getMessage());
-			}catch (IOException e) {
-				modelMap.put("success", false);	
+			} catch (IOException e) {
+				modelMap.put("success", false);
 				modelMap.put("errMsg", e.getMessage());
 			}
 			return modelMap;
-			
-		}else {
+
+		} else {
 			modelMap.put("success", false);
 			modelMap.put("errMsg", "请输入店铺信息");
 			return modelMap;
 		}
-		
+
 	}
-	
+
 //	private static void inputStreamToFile(InputStream ins,File file) {
 //		FileOutputStream os = null;
 //		try {
